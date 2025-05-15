2b1d607395a45fdd9467cfa51aeba75590a471f5
Saugat Bhandari <57620639+saugat23@users.noreply.github.com>
2025-05-15 13:03:20 +0545

    Update autostart.sh

--- suckless/autostart.sh
+++ suckless/autostart.sh
@@ -2,11 +2,14 @@
 feh --bg-fill --randomize ~/Pictures/wallpapers/*
 
 #Setup Picom
-"$HOME"/.config/picom/build/src/./picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 500 &
+picom --config ~/.config/picom/picom.conf &
+redshift -O 4800 &
 
 #Exec slstatus
 exec slstatus &
 nm-applet &
 blueman-applet &
 xfce4-clipman &
+volumeicon &
+xfce4-notee &
 
